114dd3de566cee4c5eaee0fbdeb2eeb1ef4bcbef
bverschueren <bverschueren>
2015-05-19 18:30:56 +0000

    write to /dev/tty instead of printing bell character

--- flash.py
+++ flash.py
@@ -1,10 +1,12 @@
-import weechat
 import sys
+import weechat
 
 weechat.register("flash", "bverschueren", "1.0", "GPL3", "flash/highlight", "", "")
 
 def flash_cb(data, signal, signal_data):
-	sys.stdout.write("\007")
+	tty = open('/dev/tty', 'w')
+	tty.write('\a')
+	tty.close()
 	return weechat.WEECHAT_RC_OK
 
 hook = weechat.hook_signal("weechat_highlight", "flash_cb", "")
